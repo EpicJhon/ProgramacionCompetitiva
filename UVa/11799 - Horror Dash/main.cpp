859bcf07f0114e8c12f19b40530bf45f47c7992b
EpicJhon <epicjhon1994@gmail.com>
2016-04-12 23:37:34 -0500

    11799 - Horror Dash

---
+++ UVa/11799 - Horror Dash/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#ifdef IDE
+
+#include <fstream>
+
+#endif
+
+using namespace std;
+
+/*
+ * 11799 - Horror Dash
+ */
+
+int main() {
+
+#ifdef IDE
+    std::ifstream in("tests");
+    std::cin.rdbuf(in.rdbuf()); //redirect std::cin to tests!
+    std::ofstream out("result");
+    std::cout.rdbuf(out.rdbuf()); //redirect std::cout to result!
+#endif
+
+    long tests, monsters, speed, max;
+
+    cin >> tests;
+
+    for (int i = 1; i <= tests; i++) {
+        // reset max
+        max = -1;
+        cin >> monsters;
+        // get week a
+        for (int j = 0; j < monsters; j++) {
+            cin >> speed;
+            max = (speed > max) ? speed : max;
+        }
+
+        cout << "Case " << i << ": " << max << endl;
+    }
+
+
+    return 0;
+}
